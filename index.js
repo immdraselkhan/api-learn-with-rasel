5dc626ed8f21def173ba576fcfe38889170d29f1
Md Rasel Khan <immdraselkhan@gmail.com>
2022-10-27 23:51:30 +0600

    all done

--- index.js
+++ index.js
@@ -9,6 +9,40 @@ app.get('/', (req, res) => {
   res.send('Server is running :)');
 });
 
+const courses = require('./data/data.json');
+
+app.get('/courses', (req, res) => {
+  res.send(courses);
+});
+
+app.get('/courses/popular', (req, res) => {
+  const popular = courses.data.filter(course => course.rating == '4.0') || [];
+  res.send(popular);
+});
+
+app.get('/courses/categories', (req, res) => {
+  const categories = [...new Set(courses.data.map(course => course.category))]
+  res.send(categories);
+})
+
+app.get('/coursesbycategory/:categoryName', (req, res) => {
+  const id = req.params.categoryName;
+  const coursesByCategory = courses.data.filter(course => course.category == id) || [];
+  res.send(coursesByCategory);
+});
+
+app.get('/course/:courseSlug', (req, res) => {
+  const slug = req.params.courseSlug;
+  const course = courses.data.find(course => course.slug == slug) || {};
+  res.send(course);
+});
+
+app.get('/checkout/:courseSlug', (req, res) => {
+  const slug = req.params.courseSlug;
+  const premiumAccess = courses.data.find(course => course.slug == slug) || {};
+  res.send(premiumAccess);
+});
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
